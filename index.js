ecb0e645808ea837c8476c0faae7a03b8250d966
kevinyhong <kevinyaohuiong@gmail.com>
2018-07-17 18:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+<<<<<<< HEAD
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object)
+=======
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  var newObj = obj
+>>>>>>> 91a1fc33143da19e5ec462d9d4b7b335687b6bf6
   newObj[key] = value
   return newObj
 }
 
+<<<<<<< HEAD
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
@@ -21,3 +29,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+=======
+>>>>>>> 91a1fc33143da19e5ec462d9d4b7b335687b6bf6
